256eba2a15177eead953e654763caeb3dd2bcd72
haShinui <rafaschlieper@gmail.com>
2024-09-20 21:48:00 +0200

    readying up for deployment of both django and react, seperate service, first django 3

--- backend/build.sh
+++ backend/build.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 # Exit on error
 set -o errexit
-cd backend
+echo "Failed to install requirements"; pwd; ls -al; exit 1;
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
+
+# Check for issues before proceeding (optional but recommended)
+python manage.py check
 
 # Convert static asset files
 python manage.py collectstatic --no-input
