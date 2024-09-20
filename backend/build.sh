fb756c056ab0219c071f896023ec9a07be602392
haShinui <rafaschlieper@gmail.com>
2024-09-20 21:49:13 +0200

    readying up for deployment of both django and react, seperate service, first django 2

--- backend/build.sh
+++ backend/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Exit on error
 set -o errexit
-echo "Failed to install requirements"; pwd; ls -al; exit 1;
+echo "Failed to install requirements"; pwd; ls -al;
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
